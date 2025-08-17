3a078347d47e27da658e2a24e175a2e9f35c8c7a
shashwatsrivastavaa <shrivastavaharishankar14@gmail.com>
2025-08-17 11:49:06 +0530

    Time: 5 ms (19.95%) | Memory: 8.5 MB (53.78%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+          int temp=0;
+        while(x!=0){
+            int digit= x%10;
+             if((temp<INT_MIN/10)||(temp>INT_MAX/10)){
+                  return 0;
+             }
+            temp=(temp*10)+digit;
+            x=x/10;}
+
+            
+            return temp;
+}
+        
+    
+};
