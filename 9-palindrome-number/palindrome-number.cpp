562518ff7e874c9127c355ed85c85369b935842b
Shashwat Srivastava <shrivastavaharishankar14@gmail.com>
2025-08-18 10:15:51 +0530

    Time: 8 ms (11.07%) | Memory: 8.6 MB (67.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
         int temp2=0;
         while(x>0){
         int a=x%10;
-        if (temp2 > (INT_MAX - a) / 10) return false;
+        if ((temp2<INT_MIN/10) ||(temp2>INT_MAX/10)) return false;
 
          temp2=temp2*10+a;
         x=x/10;
