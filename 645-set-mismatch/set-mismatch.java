83dd8a3e8fc4ca82ac3f9450921b519d48dd26dd
Shashwat Srivastava <shrivastavaharishankar14@gmail.com>
2026-06-20 22:15:05 +0530

    Time: 25 ms (8.04%) | Memory: 48 MB (17.84%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -7,27 +7,27 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
         int subsum = 0;
-        List<Integer>temp = new ArrayList<>();
-        Map<Integer,Integer>mp = new HashMap<>();
-        for(int num : nums){
-            mp.put(num,mp.getOrDefault(num,0) + 1);
-        } 
-        for(int num : mp.keySet()){
-            if(mp.get(num) > 1){
-              temp.add(num);
+        List<Integer> temp = new ArrayList<>();
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+        for (int num : mp.keySet()) {
+            if (mp.get(num) > 1) {
+                temp.add(num);
             }
         }
 
-        Set<Integer>st = new HashSet<>();
-        for(int m :nums){
+        Set<Integer> st = new HashSet<>();
+        for (int m : nums) {
             st.add(m);
         }
-        for(int i : st){
-              subsum += i;
+        for (int i : st) {
+            subsum += i;
         }
-        int k= n*(n+1)/2 - subsum;
-            temp.add(k);
+        int k = n * (n + 1) / 2 - subsum;
+        temp.add(k);
 
-             return temp.stream().mapToInt(i->i).toArray(); 
+        return temp.stream().mapToInt(i -> i).toArray();
     }
-} 
+}
