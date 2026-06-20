a13e2bda3dd99c0871c64ae7c3586c1b7c158a6d
Shashwat Srivastava <shrivastavaharishankar14@gmail.com>
2026-06-20 22:07:01 +0530

    Time: 25 ms (8.04%) | Memory: 47.9 MB (25.51%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,33 @@
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int subsum = 0;
+        List<Integer>temp = new ArrayList<>();
+        Map<Integer,Integer>mp = new HashMap<>();
+        for(int num : nums){
+            mp.put(num,mp.getOrDefault(num,0) + 1);
+        } 
+        for(int num : mp.keySet()){
+            if(mp.get(num) > 1){
+              temp.add(num);
+            }
+        }
+
+        Set<Integer>st = new HashSet<>();
+        for(int m :nums){
+            st.add(m);
+        }
+        for(int i : st){
+              subsum += i;
+        }
+        int k= n*(n+1)/2 - subsum;
+            temp.add(k);
+
+             return temp.stream().mapToInt(i->i).toArray(); 
+    }
+} 
